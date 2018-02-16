fc6e6d1c26397d9db1629e8eb9b99951ffd9ad47
Thiago Marcolino <thiago.marcolino@farfetch.com>
2018-02-16 17:16:02 +0000

    First commit Akka.Net

---
+++ AkkaDotNetSolution/AkkaDotNetSolution/Actors/PlaybackActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace AkkaDotNetSolution.Actors
+{
+    public class PlaybackActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            Console.Write($"==== PlaybackActor received a message message: {message} ====");
+        }
+    }
+}
---
+++ AkkaDotNetSolution/AkkaDotNetSolution/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using AkkaDotNetSolution.Actors;
+
+namespace AkkaDotNetSolution
+{
+    class Program
+    {
+
+        private static ActorSystem movieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            using (movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem"))
+            {
+                // Actor factory
+                Props playbackActorProps = Props.Create<PlaybackActor>();
+                IActorRef playbackActorRef = movieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+                playbackActorRef.Tell("Uhhuuuu!");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
